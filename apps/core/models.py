9002d6bc04f3d3d450563296530c42d9d99a45b8
Marisa Yeung <neurosa@gmail.com>
2019-07-19 21:03:59 -0700

    first pass at Note and Notebook models

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO: use django-taggit to manage tags for Notes
+
+
+# First pass at Note model
+class Note(models.Model):
+    username = models.CharField(max_length=30)
+    text = models.CharField(max_length=300)  # TODO: use more appropriate type
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username + ' said ' + self.text
+
+
+# First pass at Notebook model
+# One notebook to many notes
+class Notebook(models.Model):
+    username = models.CharField(max_length=30)
+    title = models.CharField(max_length=25)
+    notes = models.ForeignKey(Note, on_delete=models.CASCADE(), blank=True, null=True)  # ok to have no notes
+
+    def __str__(self):
+        return self.username + "'s notebook: " + self.title
+
+# NOTE: User is a built-in model in Django, which means it's not included in
+# models.py since it automatically comes with Django "for free". It has the
+# following fields:
+
+
+# class User:
+#    username
+#    first_name
+#    last_name
+#    password
+#    email
