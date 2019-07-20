749c23d08dc604966800f5d559153a62c752cd63
Marisa Yeung <neurosa@gmail.com>
2019-07-19 21:15:13 -0700

    added to admin, fixed a model, made migrate

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 # TODO: use django-taggit to manage tags for Notes
 
+# Remember to run python manage.py makemigrations and then migrate when making changes!
+
 
 # First pass at Note model
 class Note(models.Model):
@@ -20,7 +22,7 @@ class Note(models.Model):
 class Notebook(models.Model):
     username = models.CharField(max_length=30)
     title = models.CharField(max_length=25)
-    notes = models.ForeignKey(Note, on_delete=models.CASCADE(), blank=True, null=True)  # ok to have no notes
+    notes = models.ForeignKey(Note, on_delete=models.CASCADE, blank=True, null=True)  # ok to have no notes
 
     def __str__(self):
         return self.username + "'s notebook: " + self.title
