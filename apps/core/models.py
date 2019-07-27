3612c15f21f50163fd6f17d95930d71813ee3db1
Yongjian Pan <yongjianpan@Yongjians-MacBook-Air.local>
2019-07-27 11:32:30 -0700

    Added Gravatar API

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from evernewnote import config
+from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
 
 # TODO: use django-taggit to manage tags for Notes
 
@@ -31,9 +33,25 @@ class Note(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    
+    def get_gravatar(self):
+        # Don't spend too much time worrying about this: This is the example
+        # code found online for Gravatar, which is an API to generate avatars
+        # based on email (we'll use username in this case).
+        email = self.username
+        encoded = hashlib.md5(email.encode('utf8')).hexdigest()
+        gravatar_url = "http://www.gravatar.com/avatar/" + encoded + "?d=identicon"
+        return gravatar_url
 
     def __str__(self):
         return self.user.username + "'s note: " + self.title
+
+#class TaggedItem(TaggedItemBase):
+#    content_object = models.ForeignKey('Note', on_delete=models.CASCADE)
+#
+#class Tabs(models.Model):
+#    tags = TaggableManager(through=TaggedItem)
+    
 
 
 # NOTE: User is a built-in model in Django, which means it's not included in
